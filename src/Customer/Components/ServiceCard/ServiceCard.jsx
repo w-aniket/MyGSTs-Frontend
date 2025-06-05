ebf85f85ed2b047670ce44338f2a658b23948a7a
aniket1137 <waniket011@gmail.com>
2025-06-05 14:37:10 +0530

    services user side frontend completed

---
+++ src/Customer/Components/ServiceCard/ServiceCard.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const ServiceCard = ({title, icon, iconbg, features}) => {
+  return (
+    <div className='main-service-card'>
+        <div className="main-service-header">
+
+        <div className="icon-circle" style={{backgroundColor: iconbg}}>
+            <span className="icon">{icon}</span>
+        </div>
+        <a href="#">
+          <span className="external-icon">🔗</span>
+        </a>
+        </div>
+        <h3 className='main-service-title'>{title}</h3>
+        <ul className='main-service-features'>
+            {features.map((feature, idx) => <li key={idx}>{feature}</li>)}
+        </ul>
+    </div>
+  )
+}
+
+export default ServiceCard
--- src/Customer/Pages/Services/Services.jsx
+++ src/Customer/Pages/Services/Services.jsx
@@ -1,8 +1,23 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Services.css'
 import { Typewriter } from 'react-simple-typewriter'
+import ServiceCard from '../../Components/ServiceCard/ServiceCard'
+import axios from 'axios'
 
 const Services = () => {
+  const [services, setServices] = useState([]);
+  const apiUrl = import.meta.env.VITE_API_URL;
+
+  useEffect(() => {
+    try {
+      axios.get(`${apiUrl}/api/services`).then((res) => {
+        setServices(res.data.services);
+      });
+    } catch (error) {
+      console.error("Error fetching services:", error);
+    }
+  },[])
+
   return (
     <div>
       <div className="services-intro-word">
@@ -20,6 +35,18 @@ const Services = () => {
         <p>
         We are India’s fast-growing business revival consultancy, offering a digital CA services platform designed to support entrepreneurs, startups, and industry professionals in launching and expanding their businesses seamlessly and affordably.
         </p>
+
+      </div>
+
+      <div className="main-services-container">
+        {services.map((service) => (
+          <ServiceCard key={service._id} 
+            title={service.title}
+            icon={service.icon || "🔧"} // Default icon if none provided
+            iconbg={service.iconbg || "#e0e0e0"}
+            features={service.features || []}
+          />
+        ))}
 
       </div>
       
